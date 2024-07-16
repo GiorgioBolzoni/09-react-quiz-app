ba37ab0c8ff0f904df312d90c55aa4afa2b3e5fa
GiorgioBolzoni <139789755+GiorgioBolzoni@users.noreply.github.com>
2024-07-16 17:23:53 +0200

    randomizzato ordine risposte

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -6,6 +6,10 @@ const[userAnswer, setUserAnswer] = useState([]); // memorizzo nell'array le risp
 
 const activeQuestionIndex = userAnswer.length; // se l'array è 0 la domanda sarà index 0 (=la prima) e così via
 
+// creo un nuovo array per non modificare quello originale in cui devo tenere la 1 risposta come prima perchè la identifica come quella corretta
+const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+shuffledAnswers.sort(() => Math.random() - 0.5); //mescola le domande il 50% delle volte (0.5), in quanto 'a' e 'b' (che sono sottintesi in (a,b)=>.....) genereranno il 50% delle volte un numero negativo (fino a -1) ed il 50% uno positivo (fino a 1)
+
 function handleSelectedAnswer(selectedAnswer){
     setUserAnswer((prevUserAnswer)=>{
         return [...prevUserAnswer, selectedAnswer];
@@ -16,7 +20,7 @@ function handleSelectedAnswer(selectedAnswer){
         <div className="question">
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
             <ul id="answers">                
-                {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                {shuffledAnswers.map((answer) => (
                     <li key={answer} className="answer">
                         <button onClick={() => handleSelectedAnswer(answer)}>{answer}</button>
                     </li>
