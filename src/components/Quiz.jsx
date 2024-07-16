38d7cb15681040490c6974b2745ae6f67609247f
GiorgioBolzoni <139789755+GiorgioBolzoni@users.noreply.github.com>
2024-07-16 16:58:56 +0200

    incorporato js con domande e creata base del componente quiz

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,8 +1,27 @@
-
+import { useState } from "react";
+import QUESTIONS from "../questions";
 export default function Quiz(){
+// const[activeQuestionIndex, setActiveQuestionIndex] = useState(0);  NON NECESSARIO, si può ricavare in altro modo(vedi sotto): meglio gestire meno States
+const[userAnswer, setUserAnswer] = useState([]); // memorizzo nell'array le risposte date
 
+const activeQuestionIndex = userAnswer.length; // se l'array è 0 la domanda sarà index 0 (=la prima) e così via
 
-    return <section id="quiz">
-        <p>Currently Active Question</p>
-    </section>
+function handleSelectedAnswer(selectedAnswer){
+    setUserAnswer((prevUserAnswer)=>{
+        return [...prevUserAnswer, selectedAnswer];
+    });
+};
+
+    return <div id="quiz">
+        <div className="question">
+            <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+            <ul id="answers">                
+                {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+                    <li key={answer} className="answer">
+                        <button onClick={() => handleSelectedAnswer(answer)}>{answer}</button>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    </div>
 }
