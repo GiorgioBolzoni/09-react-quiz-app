5afce904ce8b60793d82b8b7a55a74c7383bec1d
GiorgioBolzoni <139789755+GiorgioBolzoni@users.noreply.github.com>
2024-07-16 17:38:46 +0200

    implementato conclusione quiz con schermata apposita

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,14 +1,12 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 export default function Quiz(){
 // const[activeQuestionIndex, setActiveQuestionIndex] = useState(0);  NON NECESSARIO, si può ricavare in altro modo(vedi sotto): meglio gestire meno States
 const[userAnswer, setUserAnswer] = useState([]); // memorizzo nell'array le risposte date
 
 const activeQuestionIndex = userAnswer.length; // se l'array è 0 la domanda sarà index 0 (=la prima) e così via
-
-// creo un nuovo array per non modificare quello originale in cui devo tenere la 1 risposta come prima perchè la identifica come quella corretta
-const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
-shuffledAnswers.sort(() => Math.random() - 0.5); //mescola le domande il 50% delle volte (0.5), in quanto 'a' e 'b' (che sono sottintesi in (a,b)=>.....) genereranno il 50% delle volte un numero negativo (fino a -1) ed il 50% uno positivo (fino a 1)
+const quizIsComplete = activeQuestionIndex === QUESTIONS.length; // completato se corrisponde al numero di domande
 
 function handleSelectedAnswer(selectedAnswer){
     setUserAnswer((prevUserAnswer)=>{
@@ -16,7 +14,22 @@ function handleSelectedAnswer(selectedAnswer){
     });
 };
 
-    return <div id="quiz">
+if(quizIsComplete){
+    return (
+        <div id="summary">
+            <img src={quizCompleteImg} alt="Trophy icon" />
+            <h2>Quiz Completed!</h2>
+        </div>
+);
+}
+// NB: inserisco questa logica di shuffle qui stotto l'if così che venga eseguito solo quando manca ancora 1 domanda da visualizzare
+
+// creo un nuovo array per non modificare quello originale in cui devo tenere la 1 risposta come prima perchè la identifica come quella corretta
+const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+shuffledAnswers.sort(() => Math.random() - 0.5); //mescola le domande il 50% delle volte (0.5), in quanto 'a' e 'b' (che sono sottintesi in (a,b)=>.....) genereranno il 50% delle volte un numero negativo (fino a -1) ed il 50% uno positivo (fino a 1)
+
+    return( 
+    <div id="quiz">
         <div className="question">
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
             <ul id="answers">                
@@ -28,4 +41,5 @@ function handleSelectedAnswer(selectedAnswer){
             </ul>
         </div>
     </div>
+    );
 }
