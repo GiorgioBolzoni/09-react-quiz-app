c7a5b961147370e2aaf825c0affe0784df11d9ae
GiorgioBolzoni <139789755+GiorgioBolzoni@users.noreply.github.com>
2024-07-18 16:48:28 +0200

    implementata classe dinamica alle risposte per colorare in base alla correttezza della risposta

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -4,17 +4,33 @@ import QUESTIONS from "../questions";
 import QuestionTimer from "./QuestionTimer";
 import quizCompleteImg from "../assets/quiz-complete.png";
 export default function Quiz(){
-// const[activeQuestionIndex, setActiveQuestionIndex] = useState(0);  NON NECESSARIO, si può ricavare in altro modo(vedi sotto): meglio gestire meno States
+
+const [answerState, setAnswerState] = useState('');
+    // const[activeQuestionIndex, setActiveQuestionIndex] = useState(0);  NON NECESSARIO, si può ricavare in altro modo(vedi sotto): meglio gestire meno States
 const[userAnswer, setUserAnswer] = useState([]); // memorizzo nell'array le risposte date
 
-const activeQuestionIndex = userAnswer.length; // se l'array è 0 la domanda sarà index 0 (=la prima) e così via
+const activeQuestionIndex = answerState === '' ? userAnswer.length : userAnswer.length -1; // se l'array è 0 la domanda sarà index 0 (=la prima) e così via
 const quizIsComplete = activeQuestionIndex === QUESTIONS.length; // completato se corrisponde al numero di domande
 
 const handleSelectedAnswer = useCallback (function handleSelectedAnswer(selectedAnswer){
+    setAnswerState ('answered');
     setUserAnswer((prevUserAnswer)=>{
         return [...prevUserAnswer, selectedAnswer];
     });
-}, []);
+
+    setTimeout(()=>{
+        if(selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
+            setAnswerState ('correct');
+        } else {
+            setAnswerState ('wrong');
+        }
+
+        setTimeout(()=>{
+            setAnswerState('');
+        }, 2000); // dopo un secondo resetto lo stato di risposta
+    }, 1000); 
+
+}, [activeQuestionIndex]);
 
 const handleSkipAnswer = useCallback(() => handleSelectedAnswer(null), [handleSelectedAnswer]);
 
@@ -42,11 +58,26 @@ shuffledAnswers.sort(() => Math.random() - 0.5); //mescola le domande il 50% del
              />
             <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
             <ul id="answers">                
-                {shuffledAnswers.map((answer) => (
+                {shuffledAnswers.map((answer) => {
+                    const isSelected = userAnswer[userAnswer.length - 1] === answer;
+                    let cssClasses = '';
+
+                    if(answerState === 'answered' && isSelected){
+                        cssClasses = 'selected';
+                    }
+
+                    if((answerState === 'correct' || answerState === 'wrong') && isSelected){
+                        cssClasses = answerState;
+                    }
+
+                return    (
                     <li key={answer} className="answer">
-                        <button onClick={() => handleSelectedAnswer(answer)}>{answer}</button>
+                        <button onClick={() => handleSelectedAnswer(answer)} className={cssClasses}>
+                            {answer}
+                        </button>
                     </li>
-                ))}
+                    )
+                })}
             </ul>
             
         </div>
